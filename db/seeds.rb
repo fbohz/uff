533a6f469c1fefe0ff15387742021f0b6d783d5c
Felipe <fb.dc.col@gmail.com>
2019-10-02 13:54:42 -0400

    seeds added for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bogota = Location.new(city: "Bogota", country: "Colombia", continent: "South America")
+
+candelaria1 = Wall.new(active: true, address: "la candelaria", description: "colorful art collaboration of Rodez and Pez in 2018")
+
+candelaria1.location = bogota
+
+rodez = Artist.new(name: "Rodez", 
+    bio: "Rodez is a visual artist who has created and exhibited graphic art and public works in several countries worldwide. He has published over 50 books for children and adults and he has been illustrator for magazines and newspapers in America and Europe. His work is recognized through publications and exhibitions and he has obtained several awards for this. Rodez is the father of two other prominent Colombian artists, Nomanda and Malegria",
+    hometown: "Bogota",
+    instagram: "https://www.instagram.com/rodez.art/",
+    website: "https://www.facebook.com/pages/category/Artist/Rodez-217105104978132/"
+    )
+pez = Artist.new(
+    name: "Pez", 
+    bio: "Spanish graffitti artist. Pez started painting in 1999 in his hometown Barcelona. Specifically, he got into street art writing his signature, which soon took the shape of a fish. 'Little by little his tags turned into the shape of a smiling blue fish, and he became a celebrity throughout Barcelona'.",
+    hometown: "Barcelona",
+    instagram: "https://www.instagram.com/pezbarcelona/?hl=en",
+    website: "https://el-pez.com/"
+    )    
+t1 = Tag.new(name: "fantasy")
+t2 = Tag.new(name: "abstract") 
+t3 = Tag.new(name: "urban")   
+
+candelaria1.artists += [rodez, pez]
+candelaria1.save
+pez.artist_walls.last.contribution_details = "painted the fishes"
