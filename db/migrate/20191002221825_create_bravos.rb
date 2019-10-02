d5908c4a16184a8e259ef76c68c24034d4a4cde4
Felipe <fb.dc.col@gmail.com>
2019-10-02 18:19:53 -0400

    removed bravo_count attribute no need to save to db  will be model method

--- db/migrate/20191002221825_create_bravos.rb
+++ db/migrate/20191002221825_create_bravos.rb
@@ -1,7 +1,6 @@
 class CreateBravos < ActiveRecord::Migration[6.0]
   def change
     create_table :bravos do |t|
-      t.integer :bravo_count
       t.belongs_to :wall, null: false, foreign_key: true
       t.belongs_to :user, null: false, foreign_key: true
 
