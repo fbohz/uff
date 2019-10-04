a8983f09bd1e40c8c605e2b09225625b809fcf53
Felipe <fb.dc.col@gmail.com>
2019-10-04 14:53:58 -0400

    cont some controller tidbits for reminders on nested attr

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-    has_many :artist_walls
+    has_many :artist_walls, dependent: :destroy
     has_many :walls, through: :artist_walls
     has_many :locations, through: :walls
     has_many :bravos, through: :walls
