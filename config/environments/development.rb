57241ace3a128d90dd85c8fffdcb17e19e80c40e
Felipe <fb.dc.col@gmail.com>
2019-10-06 13:25:17 -0400

    devise forgot pass ok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,19 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  #devise - url options.
+  #devise - url options and devise send forget password 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password:ENV["GMAIL_PASSWORD"]
+  }
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
 
 end
