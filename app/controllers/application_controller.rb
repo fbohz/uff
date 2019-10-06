aeacb335f61f54d0424410bb9d6c4f605cc9eb99
Felipe <fb.dc.col@gmail.com>
2019-10-06 12:48:10 -0400

    devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     protected
   
     def configure_permitted_parameters
-      attributes = [:name]
+      attributes = [:name, :provider, :uid, :google_refresh_token, :google_token]
       devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
       devise_parameter_sanitizer.permit(:account_update, keys: attributes)
     end
