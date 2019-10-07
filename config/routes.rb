b14f82c2cf285e27f5c4c59c79915dee92bc3061
Felipe <fb.dc.col@gmail.com>
2019-10-07 16:40:16 -0400

    add anticipated patterns on routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
-  # resources :artists, only: [:index, :show]
+  resources :artists do
+    resources :walls, only: [:show, :index]
+  end
+
+  resources :walls do
+    resources :artists, only: [:show, :index]
+  end
+
+  resources :locations
+
 end
