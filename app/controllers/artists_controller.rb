4f8e76e7f677fcb016feb0e8a0bb73ab45852646
Felipe <fb.dc.col@gmail.com>
2019-10-09 14:48:49 -0400

    add_http protocol controller helper done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,8 @@
 class ArtistsController < ApplicationController
     before_action :set_artist, only: [:show, :edit, :update, :destroy]
 
+    # after_validation :add_http_protocol
+
     def index
     end 
 
@@ -14,7 +16,14 @@ class ArtistsController < ApplicationController
     end 
 
     def create
-        raise params.inspect
+        # raise params.inspect
+        @artist = Artist.new(artist_params)
+        add_http_protocol
+        if @artist.save
+            redirect_to artist_path(@artist)
+        else
+            render :new
+        end        
     end 
 
     def edit
@@ -30,11 +39,22 @@ class ArtistsController < ApplicationController
 
     def artist_params
        #NEED TO COMPLETE AND INCLUDE NESTED FORMS ATTRIBUTES TOO don't forget nested :id too, 
-       params.require(:artist).permit(:name, :hometown, :instagram, :website, :bio, :image) 
+       params.require(:artist).permit(:name, :hometown, :instagram, :website, :bio, :image, :wall_id, :location_id) 
     end 
 
     def set_artist
         @artist = Artist.find(params[:id])
     end
+
+    def add_http_protocol
+        array = ["#{@artist.instagram}", "#{@artist.website}"]
+        array.collect! do |url|
+            unless url[/\Ahttp:\/\//] || url[/\Ahttps:\/\//] || url.empty? 
+            url = "http://#{url}"
+            end
+         end
+        @artist.instagram = array[0]
+        @artist.website = array[1]
+     end
       
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,13 +5,13 @@ class Artist < ApplicationRecord
     validates_uniqueness_of :name
     validates :name, :hometown, length: { in: 2..20 }
     validates :bio, length: { maximum: 500 }
-    validates :website, :instagram, url: { public_suffix: true, allow_nil: true }
+    validates :website, :instagram, url: { public_suffix: true, allow_nil: true, allow_blank: true }
     has_many :artist_walls, dependent: :destroy
     has_many :walls, through: :artist_walls
     has_many :locations, through: :walls
     has_many :bravos, through: :walls
     has_many :tags, through: :walls
-    accepts_nested_attributes_for :walls
+    accepts_nested_attributes_for :walls, :locations
     has_one_attached :image
 
 end
