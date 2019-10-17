31042f0de2d0799162b99a4d0696f0c6353df13a
Felipe <fb.dc.col@gmail.com>
2019-10-17 18:51:40 -0400

    helper work for artist walls index

--- app/helpers/walls_helper.rb
+++ app/helpers/walls_helper.rb
@@ -1,3 +1,9 @@
 module WallsHelper
-    
+    def index_title(wall, artist =nil)
+        if artist && artist.walls.present?
+            content_tag(:h1, "#{artist.name} Walls:")
+        else
+            content_tag(:h1, "Latest Walls" )
+        end 
+    end
 end
