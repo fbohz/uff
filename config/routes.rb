38a762709c4ff6dc45339b940fca48287de42091
Felipe <fb.dc.col@gmail.com>
2019-10-17 16:32:58 -0400

    shallow true for index create new instead of only

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
   resources :artists do
-    resources :walls, only: [:show, :index, :new, :edit]
+    resources :walls, shallow: true
   end
 
   resources :walls 
