ada558f8ed6690fc3a830bf8cbd69d8ed61f90fc
Felipe <fb.dc.col@gmail.com>
2019-10-19 21:40:50 -0400

    taking shape index

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,23 @@ module HomeHelper
     def home_tag_list(wall)
         tags = wall.tags.limit(2)
     end 
+
+    def check_tags(model)
+        html = ''
+        if model.tags.present? 
+            html << "<a> ##{model.tags.first.name} </a>"
+        end
+        html.html_safe
+    end
+
+    def wall_artists_card(wall)
+        html = ''
+        if wall.artists.length > 1
+            html << "<b> Multiple Artist </b>Collaboration"
+        else
+            artist = wall.artists.first
+            html << link_to(artist.name, artist_path(artist)) 
+        end
+        html.html_safe
+    end 
 end
