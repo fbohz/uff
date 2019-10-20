efbb88ed47400bed26b791c424bd18ff9da927c2
Felipe <fb.dc.col@gmail.com>
2019-10-20 12:41:35 -0400

    js cleanup:

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,16 +2,3 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require_tree .
-
-// $(document).ready(function() {
-//     $('#addArtist').click(function(event){
-//     //   alert('Hooray!');
-//       event.preventDefault(); // Prevent link from following its href
-      
-//     });
-//   });
-
-
-// function getPublicJson(){
-//     return $.getJSON('./public/static.json');
-// }
