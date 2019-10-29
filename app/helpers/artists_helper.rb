22a83abe5eb83c7022d541eb01683882654af95a
Felipe <fb.dc.col@gmail.com>
2019-10-29 14:08:11 -0400

    fix insert wall image artist helper now good if no wall image

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,10 +2,8 @@ module ArtistsHelper
     def insert_wall_image_link(artist)
         html = ''
         wall = artist.walls.take
-        if wall
-            if wall.images.first.present?
+        if wall && wall.images.first.present?
                 html <<  "<img src='https://storage.googleapis.com/rails_proj/#{wall.images.first.key.to_s}' alt='Wall image'>"
-            end 
         else 
             html << "<img src='/img/wsoon.jpg' alt='Placeholder image'>"
         end
